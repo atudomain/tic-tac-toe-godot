b5bce6c7c81bc23f54c6f62dfceb57e4d19710b2
Adrian Tuzimek <tuziomek@gmail.com>
2021-08-22 13:25:46 +0200

    Improve win calculation

--- GameState.gd
+++ GameState.gd
@@ -2,28 +2,69 @@ extends Node
 
 
 onready var current_player = -1
+onready var win_coordinates = []
 
-onready var board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+var board = [
+	[0, 0, 0], 
+	[0, 0, 0], 
+	[0, 0, 0]
+]
+
+var rows_coordinates = [
+	[[0,0],[0,1],[0,2]],
+	[[1,0],[1,1],[1,2]],
+	[[2,0],[2,1],[2,2]]
+]
+
+var columns_coordinates = [
+	[[0,0],[1,0],[2,0]],
+	[[0,1],[1,1],[2,1]],
+	[[0,2],[1,2],[2,2]]
+]
+
+var diagonals_coordinates = [
+	[[0,0],[1,1],[2,2]],
+	[[0,2],[1,1],[2,0]]
+]
+
+func get_board_value(coordinates):
+	return board[coordinates[0]][coordinates[1]]
 
 func is_win():
-	var sums = []
-	for i in range(3):
-		sums.append(sum(board[i]))
-	for i in range(3):
-		var current_sum = 0
-		for j in range(3):
-			current_sum += board[j][i]
-		sums.append(current_sum)
-	var current_sum = 0
-	for x in [[0,0], [1,1], [2,2]]:
-		current_sum += board[x[0]][x[1]]
-	sums.append(current_sum)
-	current_sum = 0
-	for x in [[0,2], [1,1], [2,0]]:
-		current_sum += board[x[0]][x[1]]
-	sums.append(current_sum)
-	for x in sums:
-		if x == -3 or x == 3:
+	if win_coordinates:
+		return true
+	for row_coordinates in rows_coordinates:
+		var row_sum = sum(
+			[
+				get_board_value(row_coordinates[0]), 
+				get_board_value(row_coordinates[1]), 
+				get_board_value(row_coordinates[2])
+			]
+		)
+		if row_sum == 3 or row_sum == -3:
+			win_coordinates = row_coordinates
+			return true
+	for column_coordinates in columns_coordinates:
+		var column_sum = sum(
+			[
+				get_board_value(column_coordinates[0]), 
+				get_board_value(column_coordinates[1]), 
+				get_board_value(column_coordinates[2])
+			]
+		)
+		if column_sum == 3 or column_sum == -3:
+			win_coordinates = column_coordinates
+			return true
+	for diagonal_coordinates in diagonals_coordinates:
+		var diagonal_sum = sum(
+			[
+				get_board_value(diagonal_coordinates[0]), 
+				get_board_value(diagonal_coordinates[1]), 
+				get_board_value(diagonal_coordinates[2])
+			]
+		)
+		if diagonal_sum == 3 or diagonal_sum == -3:
+			win_coordinates = diagonal_coordinates
 			return true
 	return false
 
@@ -42,4 +83,5 @@ func sum(a):
 
 func reset():
 	current_player = -1
+	win_coordinates = []
 	board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
